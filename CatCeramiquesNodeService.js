1d6a412c82c7ca906d14f4e234d862068acf95ee
Paul-Antoine Levasseur <palevasseur@free.fr>
2014-05-10 12:17:16 +0200

    add thumbs

--- CatCeramiquesNodeService.js
+++ CatCeramiquesNodeService.js
@@ -5,60 +5,34 @@
 var express = require('express');
 var fs = require('fs');
 var url = require('url');
+var im = require('imagemagick');
 var CatCeramiques = require('./models/CatCeramiques');
 
 var config = {
-    "dev": {
+    "current":"pc",
+    "pc": {
         "photosDirectory": "../Ressources/PhotosLaBouille"
     },
     "ds211": {
         "photosDirectory": "../../../../photo/Catalogue des ceramiques"
-    },
-    "default":"dev"
+    }
 };
 
-var photoDirectory = config[config.default].photosDirectory;
+var photoDirectory = config[config.current].photosDirectory;
 var app = express();
-
-/*
-//CreateFilesList("../../../photo/Catalogue des ceramiques", function (list, nbrPhotos, nbrPhotosWihoutNum) {
-CreateFilesList("../Ressources/PhotosLaBouille", function (list, stat) {
-    var listJSON = JSON.stringify(list);
-    var statJSON = JSON.stringify(stat);
-    console.log("Ceramics catalogue has "+stat.NbrPhotos+" photos (missing piece number for "+stat.NbrPhotosWihoutNum+" photos)");
-    console.log("Server listening on port 8001 ...");
-    http.createServer(function(request, response) {
-        console.log("url="+request.url);
-        if('/'==request.url){
-            response.write("<p><h3>Usage</h3><lu><li>/list</li><li>/stat</li></lu></p>");
-            response.end();
-        }
-        else if('/list'==request.url || '/stat'==request.url){
-            switch(request.method){
-                case 'GET':
-                    response.setHeader('Access-Control-Allow-Origin', '*'); // better to set: http://localhost:8000
-                    response.writeHead(200, {"Content-Type": "application/json"});
-                    if('/list'==request.url)
-                        response.write(listJSON);
-                    if('/stat'==request.url)
-                        response.write(statJSON);
-                    response.end();
-                    break;
-            }
-        }
-        else{
-            response.statusCode = 404;
-            response.end('Not Found');
-        }
-    }).listen(8001);
-});
-*/
 
 CatCeramiques.CreateFilesList(photoDirectory, function (list, stat) {
     var listJSON = JSON.stringify(list);
     var statJSON = JSON.stringify(stat);
     console.log("Ceramics catalogue has "+stat.NbrPhotos+" photos (missing piece number for "+stat.NbrPhotosWihoutNum+" photos)");
     console.log("Server listening on port 8001 ...");
+
+    /*
+     if(!fs.existsSync(imgThumb)) {
+     fs.mkdirSync(imgThumb);
+     console.log("created directory '" + imgThumb + "'");
+     }
+     */
 
     app.get('/', function(request, response) {
         response.write("<p>Usage<br>- list: http://&lt;server&gt;/list<br>- statistics: http://&lt;server&gt;/stat<br>- repository: http://&lt;server&gt;/repo?img=&lt;image name&gt;</p>");
@@ -83,23 +57,55 @@ CatCeramiques.CreateFilesList(photoDirectory, function (list, stat) {
         var req = url.parse(request.url, true);
         if(req.query.img) {
             try {
-                var file = fs.readFileSync(photoDirectory + "/" + req.query.img);
-                response.setHeader('Access-Control-Allow-Origin', '*'); // better to set: http://localhost:8000
-                response.writeHead(200, {"Content-Type": "image/jpg"});
-                response.write(file);
-                response.end();
+                var imgSrc = photoDirectory + "/" + req.query.img;
+                var imgThumb = photoDirectory + "/thumbs/" + req.query.img;
+
+                fs.readFile(imgThumb, function (err, file){
+                    if(err) {
+                        im.resize({
+                            srcPath: imgSrc,
+                            dstPath: imgThumb,
+                            width:   64
+                        }, function(err, stdout, stderr){
+                            if (err) throw err;
+
+                            fs.readFile(imgThumb, function (err, file) {
+                                if (err) {
+                                    console.log("failed to create thumb for '" + req.query.img + "'");
+                                }
+                                else {
+                                    console.log("created thumb for '" + req.query.img + "'");
+
+                                    response.setHeader('Access-Control-Allow-Origin', '*'); // better to set: http://localhost:8000
+                                    response.writeHead(200, {"Content-Type": "image/jpg"});
+                                    response.write(file);
+                                    response.end();
+                                }
+                            });
+                        });
+                    }
+                    else {
+                        response.setHeader('Access-Control-Allow-Origin', '*'); // better to set: http://localhost:8000
+                        response.writeHead(200, {"Content-Type": "image/jpg"});
+                        response.write(file);
+                        response.end();
+                    }
+                });
+
+
             }
             catch(e) {
-                // todo: add info error = image not found
                 response.setHeader('Access-Control-Allow-Origin', '*'); // better to set: http://localhost:8000
-                response.writeHead(404, {"Content-Type": "image/jpg"});
+                response.writeHead(404, {"Content-Type": "text/html"});
+                response.write("image '" + req.query.img + "' not found !");
                 response.end();
             }
         }
         else {
             // todo: add info error = bad param
             response.setHeader('Access-Control-Allow-Origin', '*'); // better to set: http://localhost:8000
-            response.writeHead(404, {"Content-Type": "image/jpg"});
+            response.writeHead(404, {"Content-Type": "text/html"});
+            response.write("bad url, missing 'img' parameter !");
             response.end();
         }
     });
